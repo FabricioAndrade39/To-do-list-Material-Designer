a411a0df78c5518be5246c7b221c4708318ac738
FabrícioAndrade39 <fabricioandradegt@gmail.com>
2023-12-16 22:11:55 -0300

    Implementando modal usando Angular Material

--- src/app/components/todo-form/todo-form.component.ts
+++ src/app/components/todo-form/todo-form.component.ts
@@ -7,6 +7,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { TodoSignalsService } from 'src/app/services/todo-signals.service';
 import { Todo } from 'src/app/models/model/todo.model';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-todo-form',
@@ -19,12 +21,14 @@ import { Todo } from 'src/app/models/model/todo.model';
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
+    MatDialogModule,
   ],
   templateUrl: './todo-form.component.html',
   styleUrls: [],
 })
 export class TodoFormComponent {
   private todosSignalsService = inject(TodoSignalsService);
+  private dialogRefService = inject(MatDialogRef<HeaderComponent>);
   public allTodos = this.todosSignalsService.todosState();
 
   public todosForm = new FormGroup({
@@ -40,6 +44,10 @@ export class TodoFormComponent {
       const done = false;
 
       this.todosSignalsService.updateTodos({id, title, description, done});
+      this.dialogRefService.close();
     }
+  }
+  public handleCloseModal(): void {
+    this.dialogRefService.close();
   }
 }

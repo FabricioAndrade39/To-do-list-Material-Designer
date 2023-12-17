97b789175cfabc0d200e1530ee0d9d67cf0fdda0
FabrícioAndrade39 <fabricioandradegt@gmail.com>
2023-12-17 12:04:28 -0300

    Implementando card de Todo-List

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TodoCardComponent } from './components/todo-card/todo-card.component';
 import { HeaderComponent } from './components/header/header.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeaderComponent],
+  imports: [CommonModule, HeaderComponent, TodoCardComponent],
   templateUrl: './app.component.html',
-  styleUrls: [],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'todo-list-16';

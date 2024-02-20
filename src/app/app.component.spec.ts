3cf031a295526f54d470a38a252494fa217d13b6
FabrícioAndrade39 <fabricioandradegt@gmail.com>
2024-02-20 15:47:17 -0300

    Execução de testes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,26 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { TodoSignalsService } from './services/todo-signals.service';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { Todo } from './models/model/todo.model';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [AppComponent]
-  }));
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let todoSignalsService: TodoSignalsService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [AppComponent, BrowserAnimationsModule, NoopAnimationsModule],
+      providers: [TodoSignalsService],
+  });
+
+  fixture = TestBed.createComponent(AppComponent);
+  component = fixture.componentInstance;
+  todoSignalsService = TestBed.inject(TodoSignalsService);
+  fixture.detectChanges();
+
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -12,16 +28,33 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'todo-list-16' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('todo-list-16');
+  //Teste de @input()
+  it('should set @input poperty correctly', () => {
+    component.projectName = 'Testing Angular With Jest';
+    fixture.detectChanges();
+    expect(component.projectName).toEqual('Testing Angular With Jest');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  //Teste de @output() e @input()
+  it('should emit event with @output decorator correctly', () => {
+    component.projectName = 'Testing my Angular Application';
+  })
+
+  //Teste de acionamento de serviço e de um signal
+  it('should create new todo correctly and call service method', () => {
+    jest.spyOn(todoSignalsService, 'updateTodos');
+    const newTodo: Todo = {
+      id: 1,
+      title: 'Testing creating Todo',
+      description: 'Test new Todo',
+      done: true,
+    };
+    component.handleCreateTodo(newTodo);
+
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('todo-list-16 app is running!');
-  });
+
+    expect(todoSignalsService.updateTodos).toHaveBeenCalledWith(newTodo);
+    expect(component.todoSignal()).toEqual([newTodo]);
+  })
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
-import { SchoolData, SchoolService } from './services/school.service';
-import { Component, OnInit } from '@angular/core';
+import { TodoSignalsService } from './services/todo-signals.service';
+import { Component, EventEmitter, Input, Output, WritableSignal} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TodoCardComponent } from './components/todo-card/todo-card.component';
 import { HeaderComponent } from './components/header/header.component';
-import { Observable, filter, from, map, of, pipe, switchMap, zip } from 'rxjs';
-
+import { Todo } from './models/model/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -13,88 +12,24 @@ import { Observable, filter, from, map, of, pipe, switchMap, zip } from 'rxjs';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
+
+  @Input() public projectName!: string;
+  @Output() public outputEvent = new EventEmitter<string>;
 
   public title = 'todo-list-16';
-  public students: Array<SchoolData> = [];
-  public teachers: Array<SchoolData> = [];
-  private zipSchoolResponses$ = zip(
-    this.getStudentsDatas(),
-    this.getTeachersDatas(),
-  );
+  public todoSignal!: WritableSignal<Todo[]>;
 
-  private ages = of(20, 30, 40, 50, 60, 70);
-  private peopleDatas = from([
-    {name: 'Fabrício Lacerda', age: 40, profession: 'Software Deleloper'},
-    {name: 'Manuela Andrade', age: 19, profession: 'UX Designer'},
-    {name: 'Shirley Andrade', age: 25, profession: 'Scrum Master'},
-    {name: 'Sebastião', age: 50, profession: 'Software Deleloper'},
-    {name: 'Carla', age: 30, profession: 'Software Deleloper'},
-  ]);
-  private studentUserId = '2';
+  constructor(private todoSignalsService : TodoSignalsService) {}
 
-    constructor (private schoolService: SchoolService) {}
-
-  ngOnInit(): void {
-    this.getSchoolDatas();
-    this.getMultipliedAges();
-    this.getPeopleProfessions();
-    this.getSoftwareDeveloperNames();
-    this.handleFindStudentsById();
+  public handleEmitEvent(): void {
+    this.outputEvent.emit();
   }
 
-  public handleFindStudentsById(): void {
-    this.getStudentsDatas().pipe(
-      switchMap((students) => this.findStudentsById(students, this.studentUserId))
-    ).subscribe({
-      next: (response) => console.log('RETORNO ESTUDANTE FILTRADO', response),
-    });
+  public handleCreateTodo(todo : Todo): void {
+    if (todo) {
+      this.todoSignalsService.updateTodos(todo);
+      this.todoSignal = this.todoSignalsService.todosState;
+    }
   }
-
-  public findStudentsById(students: Array<SchoolData>, userId: string) {
-    return of([students.find((student) => student.id === userId)]);
-  }
-
-  public getMultipliedAges(): void {
-    this.ages.pipe(map((age) => age * age))
-    .subscribe({
-      next: (response) => console.log('IDADE MULTIPLICADA', response),
-    });
-  }
-
-  public getPeopleProfessions(): void {
-    this.peopleDatas.pipe(map((people) => people.profession))
-    .subscribe({
-      next: (response) => console.log('PROFISSÃO', response),
-    });
-  }
-
-  public getSoftwareDeveloperNames(): void {
-    this.peopleDatas
-      .pipe(
-        filter((people) => people.profession === 'Software Deleloper'),
-        map((people) => people.name)
-      )
-    .subscribe({
-      next: (response) => console.log('NOME DO DESENVOLVEDOR', response),
-    });
-  }
-
-  public getSchoolDatas(): void {
-    this.zipSchoolResponses$.subscribe({
-      next: (response) => {
-        console.log('STUDENTS', response[0]);
-        console.log('TEACHERS', response[1]);
-      },
-    });
-  }
-
-  getStudentsDatas(): Observable<Array<SchoolData>> {
-    return this.schoolService.getStudentes();
-  }
-
-  getTeachersDatas(): Observable<Array<SchoolData>> {
-    return this.schoolService.getTeachers();
-  }
-
 }
